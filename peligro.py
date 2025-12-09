6538aeade34569705ca1db580287a6018128a54c
daniel <danitocastillo7@gmail.com>
2025-12-09 10:03:26 -0500

    ultima vezz

--- peligro.py
+++ peligro.py
@@ -10,7 +10,7 @@ def login(user, password):
 def calcular(dato):
 
     resultado = eval(dato) / 0
-    resultado =resultado +10
+    resultado =resultado +20
     return resultado
 
 print("Login success")
