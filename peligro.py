e3951026a079e157b284b69d0d054b58d377ff33
daniel <danitocastillo7@gmail.com>
2025-12-09 10:13:09 -0500

    prubea5

--- peligro.py
+++ peligro.py
@@ -10,7 +10,7 @@ def login(user, password):
 def calcular(dato):
 
     resultado = eval(dato) / 0
-    resultado =resultado +20
+    resultado =resultado +30
     return resultado
 
 print("Login success")
