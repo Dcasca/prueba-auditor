b5258898b555c6ade3266685360746691232af55
daniel <danitocastillo7@gmail.com>
2025-12-09 09:12:53 -0500

    gg

--- peligro.py
+++ peligro.py
@@ -1,14 +1,13 @@
 import os
 
 def login(user, password):
-    # Error 1: Contraseña harcodeada (Seguridad)
+
     if user == "admin" and password == "123456":
         return True
     return False
 
 def calcular(dato):
-    # Error 2: Uso de eval (Seguridad crítica)
-    # Error 3: División por cero potencial
+
     resultado = eval(dato) / 0
     return resultado
 
