09c79f5987b348ba30ebbc6471934afed063a8ee
daniel <danitocastillo7@gmail.com>
2025-12-09 09:18:51 -0500

    malo

--- peligro.py
+++ peligro.py
@@ -1,10 +1,15 @@
 import os
 import sys
 
+def login(user, password):
+
+    if user == "admin" and password == "123456":
+        return True
+    return False
 
 def calcular(dato):
 
-    resultado = dato*2
+    resultado = eval(dato) / 0
     return resultado
 
 print("Login success")
